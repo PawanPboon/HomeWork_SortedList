2efb6f251f1375a21e1d7bb4e0e04b5bd5e47f39
Pawan_BU <149138396+PawanPboon@users.noreply.github.com>
2024-07-28 17:08:34 +0700

    Commit Work
    Submit Homework SortedList
    1. Added Key
    2. Added Value
    3. Show All Value

---
+++ SortedListApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortedListApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public object key;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{keyTxt.Text} has been added.");
+            sortedList.Add(keyTxt.Text,valueTxt.Text);
+            ICollection icollection = sortedList.Keys;
+            string dataName = string.Empty;
+            
+            foreach (string key in icollection)
+            {
+                dataName += sortedList[key].ToString()+"\n";
+            }
+           
+               MessageBox.Show(dataName);
+
+
+        }
+    }
+}
